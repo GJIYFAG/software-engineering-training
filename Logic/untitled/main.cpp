9f9a5f925c4f771a5ce063ea7ccdf30d3d1ee202
王耀2024051604007 <weilaiqingkeqi@qq.com>
2026-03-14 15:57:33 +0800

    完成剩余接口和书写部分注释

--- Logic/untitled/main.cpp
+++ Logic/untitled/main.cpp
@@ -8,6 +8,18 @@ public:
         EnrollmentProxy::save(studentId, taskId);
         return true;
     }
-    void gradeStudent(int teacherId, int studentId, int taskId, float grade);
-    void scheduleTask(int courseId, int teacherId, std::string semester);
+    //教师评定成绩
+    void gradeStudent(int teacherId, int studentId, int taskId, float grade) {
+        // 只有该任务的授课教师才能打分
+        EnrollmentProxy::updateGrade(studentId, taskId, grade);
+    }
+
+    // 教学秘书安排
+    void scheduleTask(int courseId, int teacherId, std::string semester) {
+        auto conn = DBConfig::connect();
+        pqxx::work tx(*conn);
+        tx.exec_params("INSERT INTO teaching_tasks (course_id, teacher_id, semester) VALUES ($1, $2, $3)",
+                       courseId, teacherId, semester);
+        tx.commit();
+    }
 };
