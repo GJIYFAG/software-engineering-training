bf674c73f023746aa9e43c6315fbc53953b2b14e
王耀2024051604007 <weilaiqingkeqi@qq.com>
2026-03-14 15:56:17 +0800

    完成初步的添加课程的函数书写，预留两个接口

--- Logic/untitled/main.cpp
+++ Logic/untitled/main.cpp
@@ -4,4 +4,10 @@
 
 class CourseController{
 public:
+    bool enrollCourse(int studentId, int taskId) {
+        EnrollmentProxy::save(studentId, taskId);
+        return true;
+    }
+    void gradeStudent(int teacherId, int studentId, int taskId, float grade);
+    void scheduleTask(int courseId, int teacherId, std::string semester);
 };
