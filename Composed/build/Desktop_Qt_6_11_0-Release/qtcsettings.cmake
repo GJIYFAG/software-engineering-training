4197db56122ff15dffc585a051724d97dce87dd0
胡艺舰2024051604006 <3960969374@qq.com>
2026-03-14 16:27:48 +0800

    发行版最小执行程序

---
+++ Composed/build/Desktop_Qt_6_11_0-Release/qtcsettings.cmake
@@ -0,0 +1,12 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "/home/ousuc/文档/GitHub/software-engineering-training2/Composed/build/Desktop_Qt_6_11_0-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_PREFIX_PATH" "/home/ousuc/Island_A/Code/Qt/6.11.0/gcc_64" CACHE "PATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
+set("QT_MAINTENANCE_TOOL" "/home/ousuc/Island_A/Code/Qt/MaintenanceTool" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_COLOR_DIAGNOSTICS" "ON" CACHE "BOOL" "" FORCE)
+set("CMAKE_C_COMPILER" "/usr/bin/gcc" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "/usr/bin/g++" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "/home/ousuc/Island_A/Code/Qt/6.11.0/gcc_64/bin/qmake" CACHE "FILEPATH" "" FORCE)
